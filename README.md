bca26e7925283d0c0545db8c251b0f5f9c2ba566
wass <wassim.filali@gmail.com>
2021-04-18 13:41:04 +0200

    get request on startup

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ sends all mqtt "request" messages over the serial and received serial messages b
 # Documentation
 
 * [Home Smart Mesh - M5Stack Atom](https://www.homesmartmesh.com//docs/microcontrollers/esp32/m5stack-atom/)
+
+# states
+* switch on
+    * red: waiting wifi
+    * blue: waiting mqtt
+    * green: mqtt connected
+    * off: response from STM32
+* button press
+    * green: status requested
+    * off  : response received and forwarded
--- src/main.cpp
+++ src/main.cpp
@@ -44,27 +44,33 @@ void connect() {
   Serial.print("checking wifi...");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    delay(1000);
+    delay(100);
   }
   M5.dis.drawpix(0, CRGB(0,0,50));
 
   Serial.print("\nmqtt connecting...");
   while (!mqtt.connect(mqtt_client_id)) {
     Serial.print(".");
-    delay(1000);
+    delay(100);
   }
   Serial.println("\nconnected!");
   M5.dis.drawpix(0, CRGB(50,0,0));
 
   mqtt.subscribe(rx_topic);
-  String payload = "{\"status\":\"running\",\"time\":"+String(millis())+"}";
-  mqtt.publish(main_topic, payload.c_str());
+  //String payload = "{\"status\":\"running\",\"time\":"+String(millis())+"}";
+  //mqtt.publish(main_topic, payload.c_str());
+}
+
+void get_status(){
+  Serial1.println("{\"get\":\"status\"}");
+  M5.dis.drawpix(0, CRGB(0,0,50));//green : status requested
 }
 
 void mqtt_loop(){
   mqtt.loop();
   if (!mqtt.connected()) {
     connect();
+    get_status();
   }
 }
 
@@ -72,6 +78,7 @@ void process_serial_2_mqtt(String str_message){
   mqtt.publish(tx_topic, str_message);
   Serial.println("\nSerial > MQTT");
   Serial.println(str_message);
+  M5.dis.drawpix(0, CRGB(0,0,0));//running all is good stop debug ad switch led off
 }
 
 
@@ -119,13 +126,20 @@ void switch_led(){
   }
 }
 
+void blink_led(){
+  M5.dis.drawpix(0, CRGB(0,0,50));
+  Serial.println("\nButton => LED: Blue blink");
+  delay(500);
+  M5.dis.drawpix(0, CRGB(0,0,0));
+}
+
 void loop() {
   mqtt_loop();
   if (M5.Btn.wasPressed()){
-    switch_led();
+    get_status();
   }
 
-  delay(50);
+  delay(10);
   M5.update();
   if(Serial1.available()>0){
     String msg = Serial1.readString();
